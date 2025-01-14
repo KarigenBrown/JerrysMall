7b01c7ff145f2aa4426c35409f13721494ecbdd6
KarigenBrown <karigenbrown@gmail.com>
2025-01-15 00:36:57 +0100

    feat(Backend): +async, initializer and auto migration

---
+++ Backend/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Backend.Data;
+using Backend.Domains.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ProductsController(StoreContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        return await context.Products.ToListAsync();
+    }
+
+    [HttpGet("/{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return await context.Products.FindAsync(id) ?? throw new InvalidOperationException();
+    }
+}
